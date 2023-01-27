c0eabf6aa101c354a6ad569f1080efa59152259e
ackerman <julio.sergio2709@gmail.com>
2023-01-27 12:28:16 -0500

    Seting up bootstrap, fontawesome...

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+/* Font Awesome */
+import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
+
+/* Boostrap */
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
@@ -13,7 +20,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
