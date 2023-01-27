67160f6722cc73c45ec2ad72383068666ee45643
ackerman <julio.sergio2709@gmail.com>
2023-01-27 14:17:34 -0500

    Creating first typescript/react component: "Customer.tsx"

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,11 @@ function App() {
         <div className="grid">
           <div className="row">
             <div className="col">
-              <p className="h3 fw-bold text-success">
-                App Component
-              </p>
+              <p className="h3 fw-bold text-success">App Component</p>
+              <p className="fst-italic">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident, tempora esse dolores inventore vitae autem perferendis saepe ipsum quas similique, eaque commodi molestias assumenda iusto optio fugiat perspiciatis, natus repellat.</p>
+              <button className="btn btn-success btn-sm">
+                <i className="fa fa-book"/> Read More
+              </button>
             </div>
           </div>
         </div>
---
+++ src/components/Customer.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+interface IProps {
+
+}
+
+let Customer: React.FC<IProps> = ({}) => {
+    return (
+        <React.Fragment>
+            
+        </React.Fragment>
+    )
+}
+
+export default Customer;
